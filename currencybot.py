6cd0b4ba0a2cf1ed238e432e951ca3f3e46bbed5
wathika <vcwathika@gmail.com>
2015-10-23 23:04:31 -0400

    initial commit

---
+++ currencybot.py
@@ -0,0 +1,86 @@
+"""
+REDDIT CURRENCY CONVERSION BOT V0.1
+Parses submission titles for currency values and replies with a comment
+containing converted values between USD, GBP, and EUR
+using up-to-date exchange rates from the fixer.io API
+
+DEPEDENCIES:
+
+PRAW (Python Reddit API Wrapper)
+OAuth2Util
+currencyconverter library for python (available at my github profile - github.com/cp2846)
+
+"""
+
+import currencyconverter
+import praw
+import time
+import OAuth2Util
+import os
+
+subreddits = ["test"]
+
+r = praw.Reddit(user_agent = "Reddit Testing by /u/Psychovyle")
+o = OAuth2Util.OAuth2Util(r)
+
+if not os.path.isfile("posts_replied_to.txt"):
+	posts_replied_to = []
+	print "no file"
+
+else:
+	with open("posts_replied_to.txt", "r") as f:
+		posts_replied_to = f.read()
+		posts_replied_to = posts_replied_to.split("\n")
+		posts_replied_to = filter(None, posts_replied_to)
+
+#save replied posts in txt file 
+def writeFile():
+	with open("posts_replied_to.txt", "w") as f:
+		for post_id in posts_replied_to:
+			f.write(post_id + "\n")
+							
+#generate and post reply
+def generateComment(submission, results):
+	output = ""
+	count = 0
+	
+	for result in results:
+		type = result[0]
+		value = result[1]
+		converted_values = currencyconverter.convert(type,value)
+		output += "\n\n"+converted_values
+		count += 1
+		
+	try:
+		submission.add_comment("Detected ("+str(count)+") currency values in title:\n\n"+output)
+		print "Successfully added comment on submission "+submission.id
+	except:
+		print "Error encountered on submission "+submission.id
+		
+
+	
+def runBot():
+	o.refresh(force=True)
+	
+	for subreddit in subreddits:
+		print "Searching submissions in r/"+subreddit
+		s = r.get_subreddit(subreddit)
+		submissions = s.get_hot(limit=25)
+		
+		for submission in submissions:
+		
+			title_text = submission.title.encode("utf-8")
+			detected_currency = currencyconverter.parseString(title_text)
+			
+			if len(detected_currency) > 0 and submission.id not in posts_replied_to:
+				print "found one! attempting to reply... "
+				generateComment(submission, detected_currency)
+				posts_replied_to.append(submission.id)
+		
+	writeFile()
+	print "Sleeping... "
+	time.sleep(3600)
+
+	
+while True:
+	runBot()
